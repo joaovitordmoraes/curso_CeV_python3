a9620561dbb956b75e17ae3086cb7b062d815494
João Vitor Moraes <32316025+joaovitordmoraes@users.noreply.github.com>
2018-10-05 17:12:47 -0300

    Correção exercicio 68

--- mundo 02/exercicio68.py
+++ mundo 02/exercicio68.py
@@ -9,23 +9,29 @@ print('VAMOS JOGAR PAR OU ÍMPAR?')
 print('=-'*16)
 v = cont = s = 0
 c = randint(0, 10)
-escolha = ''
+escolha = comp = ''
 resultado = ''
 while True:
     v = int(input('Diga um valor: '))
     escolha = str(input('Par ou Ímpar? [P/I] ')).upper().strip()
     s = v + c
     if s % 2 == 0:
+        comp = 'P'
         resultado = 'PAR'
     else:
+        comp = 'I'
         resultado = 'ÍMPAR'
+    print('-'*32)
     print(f'Você jogou {v} e o computador {c}. Total de {s} -> RESULTADO {resultado}')
-    if escolha == resultado:
+    print('-'*32)
+    if escolha == comp:
         print('Você VENCEU!')
         print('Vamos jogar novamente....')
+        print('=-' * 16)
         cont += 1
     else:
         print('Você PERDEU!')
         print(f'GAME OVER! Você venceu {cont} vezes.')
+        print('=-' * 16)
         break
 
